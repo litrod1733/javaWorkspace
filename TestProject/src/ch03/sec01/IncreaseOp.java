b2ee82a98cd76445fdea9121c55c7b7f5884456a
inseongLee <litrod1733@naver.com>
2026-01-28 10:35:34 +0900

    update(IncreaseOp): 증감 연산자 실습

---
+++ TestProject/src/ch03/sec01/IncreaseOp.java
@@ -0,0 +1,23 @@
+package ch03.sec01;
+
+public class IncreaseOp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 1;
+		int y = 1;
+		
+		System.out.println("x = 1");
+		System.out.println("y = 1");
+		
+		int result1 = ++x + 10;
+		int result2 = y++ + 10;
+		
+		System.out.println("++x + 10 = " + result1);
+		System.out.println("y++ + 10 = " + result2);
+		
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+	}
+
+}
